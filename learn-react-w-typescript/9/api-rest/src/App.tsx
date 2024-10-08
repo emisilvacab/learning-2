b35d89332104a966875d909720ce95d90fafba62
Emiliano Silva <emisilvacab@gmail.com>
2024-10-08 12:53:00 -0300

    9 with react router

--- learn-react-w-typescript/9/api-rest/src/App.tsx
+++ learn-react-w-typescript/9/api-rest/src/App.tsx
@@ -1,7 +1,17 @@
 import { PostsPage } from "./posts/PostsPage";
+import { createBrowserRouter, RouterProvider, defer } from "react-router-dom";
+import { getPosts } from "./posts/getPosts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PostsPage />,
+    loader: async () => defer({ posts: getPosts() })
+  },
+]);
 
 function App() {
-  return <PostsPage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
