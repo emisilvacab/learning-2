f154b331d8dc55a0633def6487d51e3d241df4b2
Emiliano Silva <emisilvacab@gmail.com>
2024-10-15 17:06:41 -0300

    7 with headless

---
+++ real-world-nextjs/7-ui-frameworks/headless-ui/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}'],
+  darkMode: 'class',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
