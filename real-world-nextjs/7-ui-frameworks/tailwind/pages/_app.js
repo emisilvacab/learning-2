68fb9cf16e7d014ae759716a5b064c5bcf0b6a1a
Emiliano Silva <emisilvacab@gmail.com>
2024-10-15 16:40:34 -0300

    7 toggle color with tailwind

--- real-world-nextjs/7-ui-frameworks/tailwind/pages/_app.js
+++ real-world-nextjs/7-ui-frameworks/tailwind/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import TopBar from "../components/TopBar";
+import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // attribute="class" will set a "dark" CSS class
+    // to the main <html> tag
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-gray-900 bg-gray-50 w-full min-h-screen">
+        <TopBar />
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
--- real-world-nextjs/7-ui-frameworks/tailwind/tailwind.config.js
+++ real-world-nextjs/7-ui-frameworks/tailwind/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}'],
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
